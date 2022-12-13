3bfe9cfb49c6105fa85ee8898a328efaeb4a1edf
unknown <manfungfung@yahoo.com.hk>
2022-12-13 17:25:48 +0800

    Correction

---
+++ lab06/22572417/lab_6393387A_1.py
@@ -0,0 +1,20 @@
+FILE = "lab06/src/00000000/message.txt"
+
+def findMaxCharInString( astring ):
+    maxCharIndex = 0
+    charCounts = [0]*26 # initialize 26 letter counters
+
+    return maxCharIndex
+
+def getStringFromFile( filename ):
+    ret = ""
+    return ret
+
+def main():
+    message = getStringFromFile(FILE)
+    print("Message is: {}".format(message))
+    print("The index with max freq: {}".format(
+        findMaxCharInString(message)))
+
+if __name__ == "__main__":
+    main()
