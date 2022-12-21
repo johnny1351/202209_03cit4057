2a981d494ca2a55c66268f08e0c95d75b2c799c6
unknown <manfungfung@yahoo.com.hk>
2022-12-21 15:51:04 +0800

    Correction

--- lab06/22572417/lab_6393387A_1.py
+++ lab06/22572417/lab_6393387A_1.py
@@ -2,19 +2,29 @@ FILE = "lab06/src/00000000/message.txt"
 
 def findMaxCharInString( astring ):
     maxCharIndex = 0
-    charCounts = [0]*26 # initialize 26 letter counters
-
+    charCounts = [0]*26 #initialize 26 letter counters
+    for c in astring:
+        c = c.lower()
+        if 'a' <= c <= 'z':
+            charCounts[ord(c)-ord('a')] += 1
+    
+    maxCharCount = max(charCounts)
+    maxCharIndex = charCounts.index(maxCharCount)
     return maxCharIndex
 
 def getStringFromFile( filename ):
     ret = ""
+    with open(filename, "r") as fp:
+        ret = fp.read()
     return ret
 
 def main():
     message = getStringFromFile(FILE)
     print("Message is: {}".format(message))
-    print("The index with max freq: {}".format(
-        findMaxCharInString(message)))
-
+    index = findMaxCharInString(message)
+    char = chr(97+index)
+    print("The Index with max freq: {}".format(index))
+    print("The Char with max freq: {}".format(char))
+    
 if __name__ == "__main__":
     main()
